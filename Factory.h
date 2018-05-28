832254b89d44a61bfeee389463b64500f9b1ff6d
Assaf-Haik-Barouch <Assaf98@gmail.com>
2018-05-28 16:14:31 +0200

    added all fields (for now)

--- Factory.h
+++ Factory.h
@@ -3,18 +3,38 @@
 
 #include <pthread.h>
 #include <list>
+#include <unordered_map>
 #include "Product.h"
 
 class Factory{
 private:
     //map of threads by id
-    //unordered_map<int, pthread_t*> threads_map;
+    std::unordered_map<int, pthread_t*> threads_map;
 
     //flag that is true when the returning service is open
-    //bool is_returning_open
+    bool is_returning_open;
+
+    //flag that is true when the factory is open
+    bool is_factory_open;
 
     //list of all available products from oldest produced to newest
-    //std::list<Product> available_products
+    std::list<Product> available_products;
+
+    /*list of all the filed thefts in the order they happened, each element in the list is a pair of
+    stolen Product and the fake_id of the thief who stole it.*/
+    std::list<std::pair<Product, int>> thefts;
+
+    //lock for the factory
+    pthread_mutex_t factory_lock;
+
+    //condition vars for the different thread types that use condition vars
+    pthread_cond_t thieves_condition;
+    pthread_cond_t production_condition;
+    pthread_cond_t companies_condition;
+
+    //counters for thread types that need them
+    int thieves_counter;
+    int companies_counter;
 
 public:
     Factory();
